ddee8adff221b30daa2f8e827405c0618a92dc8b
luisKonopacki <luis.konopacki@dataprom.com>
2021-08-10 16:07:40 -0300

    primeira comunicacao com back

--- src/views/login.js
+++ src/views/login.js
@@ -3,16 +3,26 @@ import Card from '../components/card'
 import FormGroup from '../components/form-group'
 import { withRouter } from 'react-router-dom'
 
+import axios from 'axios'
+
 class Login extends React.Component{
 
     state = {
         email: '',
-        senha: ''
+        senha: '',
+        menssagemErro: null
     }
     
-    entrar = () =>{
-        console.log("Email: ", this.state.email);
-        console.log("Senha: ", this.state.senha);
+    entrar = async () =>{
+        axios
+            .post('http://localhost:8080/api/usuarios/autenticar', {
+                email: this.state.email,
+                senha: this.state.senha
+            }).then( response => {
+                this.props.history.push('/home');
+            }).catch(erro => {
+                this.setState({menssagemErro: erro.response.data})
+            })
     }
 
     prepareCadastrar = () => {
@@ -25,6 +35,9 @@ class Login extends React.Component{
                 <div className="col-md-6" style={{position: 'relative', left: '300px'}}>
                     <div className="bs-docs-section">
                         <Card title="Login">
+                            <div className="row">
+                                <span>{this.state.menssagemErro}</span>
+                            </div>
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="bs-component">
