2bad3428816d7502b635b0812bb9b584b6b3a42a
fernandokonopacki <fernando.konopacki@gmail.com>
2021-08-13 06:47:08 -0300

    inserindo consulta lancamento

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Login from '../views/login'
 import Home from '../views/home'
 import CadastroUsuario from '../views/cadastroUsuario'
+import ConsultaLancamentos from '../views/consulta-lancamentos'
 
 import {Route, Switch, HashRouter} from 'react-router-dom'
 
@@ -12,7 +13,8 @@ function Rotas(){
             <Switch>
                 <Route path="/home" component={Home} />  
                 <Route path="/login" component={Login} />  
-                <Route path="/cadastroUsuario" component={CadastroUsuario}  />            
+                <Route path="/cadastroUsuario" component={CadastroUsuario}  /> 
+                <Route path="/consulta-lancamentos" component={ConsultaLancamentos}  />            
             </Switch>
         </HashRouter>
     )
